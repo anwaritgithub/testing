1280264b60c55cec0ea4cb82a7a65e3f4ea2ead8
anwar <anwaruday790@gmail.com>
2023-08-05 07:05:36 +0530

    commit jhb

---
+++ src/shammi/venki.java
@@ -0,0 +1,21 @@
+package shammi;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class venki {
+
+    public static void main(String[] args) {
+
+
+       // public static void main(String[] args) {
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\P VENKATESH\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.facebook.com/");
+
+
+
+
+
+    }
+}
